f68cf80b27a2b7721465b0ca9abc6fd7b12d9c05
Raman Agrawal <iramanagrawal@gmail.com>
2024-05-27 09:29:07 +0530

    ± Api ,removed setAuth function

--- src/services/Api.tsx
+++ src/services/Api.tsx
@@ -4,12 +4,4 @@ const Api = axios.create({
   baseURL: "https://apis.allsoft.co/api/documentManagement",
 });
 
-export const setAuthToken = (token: string) => {
-  if (token) {
-    Api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-  } else {
-    delete Api.defaults.headers.common["Authorization"];
-  }
-};
-
 export default Api;
