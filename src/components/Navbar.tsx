77369205c260a2ad541f372b4c12022b07198c49
Raman Agrawal <iramanagrawal@gmail.com>
2024-05-27 09:30:11 +0530

    +simple nav

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+// src/components/Navbar.tsx
+import React from 'react';
+
+const Navbar: React.FC = () => {
+  return (
+    <nav>
+      <h2>Admin Panel</h2>
+    </nav>
+  );
+};
+
+export default Navbar;
